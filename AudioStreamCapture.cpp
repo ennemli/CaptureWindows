6d81c9abe0f2453cce4cc4ee32c07162644f5756
Mohsine Ennemli <ennemli.moh55@gmail.com>
2025-03-22 07:55:24 +0000

    Reimplemented AudioStreamCapture to capture audio from windows

--- AudioStreamCapture.cpp
+++ AudioStreamCapture.cpp
@@ -1,50 +1,123 @@
-////AudioStreamCapture.cpp
-//#include "AudioStreamCapture.h"
-//#include <iostream>
-//
-//bool AudioStreamCapture::Initialize() {
-//	// Get the default audio device
-//	HRESULT hr = CoCreateInstance(__uuidof(MMDeviceEnumerator), nullptr,CLSCTX_ALL,__uuidof(IMMDeviceEnumerator),(void**)&m_deviceEnumerator);
-//	if (FAILED(hr)) {
-//		std::cerr << "Failed to create MMDeviceEnumerator: " << std::hex << hr << std::endl;
-//		return false;
-//	}
-//	hr = m_deviceEnumerator->GetDefaultAudioEndpoint(eRender, eConsole, &m_device);
-//	if (FAILED(hr)) {
-//		std::cerr << "Failed to get default audio endpoint: " << std::hex << hr << std::endl;
-//		return false;
-//	}
-//	
-//	// Activate the device
-//	hr = m_device->Activate(__uuidof(IAudioClient), CLSCTX_ALL, nullptr, (void**)&m_audioClient);
-//	if (FAILED(hr)) {
-//		std::cerr << "Failed to activate audio client: " << std::hex << hr << std::endl;
-//		return false;
-//	}
-//
-//	// Get Format of the audio stream
-//	hr = m_audioClient->GetMixFormat(&m_waveFormat);
-//	if (FAILED(hr)) {
-//		std::cerr << "Failed to get mix format: " << std::hex << hr << std::endl;
-//		return false;
-//	}
-//
-//	//Initialize the audio stream
-//	hr = m_audioClient->Initialize(AUDCLNT_SHAREMODE_SHARED, AUDCLNT_STREAMFLAGS_LOOPBACK, 10000000, 0, m_waveFormat, nullptr);
-//	if (FAILED(hr)) {
-//		std::cerr << "Failed to initialize audio client: " << std::hex << hr << std::endl;
-//		return false;
-//	}
-//	hr = m_audioClient->GetService(__uuidof(IAudioCaptureClient), (void**)&m_captureClient);
-//	if (FAILED(hr)) {
-//		std::cerr << "Failed to get audio capture client: " << std::hex << hr << std::endl;
-//		return false;
-//	}
-//
-//	return true;
-//}
-//
-//void AudioStreamCapture::Start(const char* outputFileName) {
+//AudioStreamCapture.cpp
+#include "AudioStreamCapture.h"
+#include <iostream>
+#include <exception>
+#include <string>
+
+AudioStreamCapture* AudioStreamCapture::instance = nullptr;
+std::mutex AudioStreamCapture::mtx;
+
+AudioStreamCapture::~AudioStreamCapture() {
+	if (m_captureClient) {
+		m_captureClient.Reset();
+	}
+	if (m_audioClient) {
+		m_audioClient.Reset();
+	}
+	if (m_device) {
+		m_device.Reset();
+	}
+	if (m_deviceEnumerator) {
+		m_deviceEnumerator.Reset();
+	}
+	if (m_waveFormat) {
+		CoTaskMemFree(m_waveFormat);
+	}
+}
+bool AudioStreamCapture::Initialize() {
+	// Get the default audio device
+	HRESULT hr = CoCreateInstance(__uuidof(MMDeviceEnumerator), nullptr,CLSCTX_ALL,__uuidof(IMMDeviceEnumerator),(void**)&m_deviceEnumerator);
+	if (FAILED(hr)) {
+		std::cerr << "Failed to create MMDeviceEnumerator: " << std::hex << hr << std::endl;
+		return false;
+	}
+	hr = m_deviceEnumerator->GetDefaultAudioEndpoint(eRender, eConsole, &m_device);
+	if (FAILED(hr)) {
+		std::cerr << "Failed to get default audio endpoint: " << std::hex << hr << std::endl;
+		return false;
+	}
+	
+	// Activate the device
+	hr = m_device->Activate(__uuidof(IAudioClient), CLSCTX_ALL, nullptr, (void**)&m_audioClient);
+	if (FAILED(hr)) {
+		std::cerr << "Failed to activate audio client: " << std::hex << hr << std::endl;
+		return false;
+	}
+
+	// Get Format of the audio stream
+	hr = m_audioClient->GetMixFormat(&m_waveFormat);
+	if (FAILED(hr)) {
+		std::cerr << "Failed to get mix format: " << std::hex << hr << std::endl;
+		return false;
+	}
+
+	//Initialize the audio stream
+	hr = m_audioClient->Initialize(AUDCLNT_SHAREMODE_SHARED, AUDCLNT_STREAMFLAGS_LOOPBACK, 10000000, 0, m_waveFormat, nullptr);
+	if (FAILED(hr)) {
+		std::cerr << "Failed to initialize audio client: " << std::hex << hr << std::endl;
+		return false;
+	}
+	hr = m_audioClient->GetService(__uuidof(IAudioCaptureClient), (void**)&m_captureClient);
+	if (FAILED(hr)) {
+		std::cerr << "Failed to get audio capture client: " << std::hex << hr << std::endl;
+		return false;
+	}
+
+	return true;
+}
+
+void AudioStreamCapture::StartStream() {
+	// Start the audio stream
+	HRESULT hr = m_audioClient->Start();
+	if (FAILED(hr)) {
+		throw std::runtime_error("Failed to start audio client: " + std::to_string(hr));
+		return;
+	}
+}
+
+void AudioStreamCapture::StopStream() {
+	// Stop the audio stream
+	HRESULT hr = m_audioClient->Stop();
+	if (FAILED(hr)) {
+		throw std::runtime_error("Failed to stop audio client: " + std::to_string(hr));
+		return;
+	}
+}
+
+std::unique_ptr<AudioData> AudioStreamCapture::CaptureAudio() {
+	// Get the next packet size
+	UINT32 framesAvailable;
+	DWORD flags;
+	HRESULT hr = m_captureClient->GetNextPacketSize(&framesAvailable);
+	if (FAILED(hr)) {
+		std::cerr << "Failed to get next packet size: " << std::hex << hr << std::endl;
+		return nullptr;
+	}
+	// Get the audio data
+	BYTE* data;
+	UINT32 numFramesAvailable;
+	hr = m_captureClient->GetBuffer(&data, &numFramesAvailable, &flags, nullptr, nullptr);
+	if (FAILED(hr)) {
+		std::cerr << "Failed to get buffer: " << std::hex << hr << std::endl;
+		return nullptr;
+	}
+	// Create the audio data object
+	std::unique_ptr<AudioData> audioData = std::make_unique< AudioData>();
+	audioData->bits_per_sample = m_waveFormat->wBitsPerSample;
+	audioData->sample_rate = m_waveFormat->nSamplesPerSec;
+	audioData->number_of_channels = m_waveFormat->nChannels;
+	audioData->number_of_frames = numFramesAvailable;
+	audioData->audio_data = data;
+	// Release the buffer
+	hr = m_captureClient->ReleaseBuffer(numFramesAvailable);
+	if (FAILED(hr)) {
+		std::cerr << "Failed to release buffer: " << std::hex << hr << std::endl;
+		return nullptr;
+	}
+	return audioData;
+}
+
+//void AudioStreamCapture::Start() {
 //	// Start the audio stream
 //	HRESULT hr = m_audioClient->Start();
 //	if (FAILED(hr)) {
@@ -83,11 +156,9 @@
 //		Sleep(10);
 //	}
 //	// Cleanup
-//	fclose(outputFile);
 //	m_audioClient->Stop();
 //	m_captureClient->Release();
 //	m_audioClient->Release();
 //	m_device->Release();
 //	m_deviceEnumerator->Release();
-//	std::cout << "Capture complete. Data saved to "<<outputFileName << std::endl;
 //}
--- AudioStreamCapture.h
+++ AudioStreamCapture.h
@@ -1,19 +1,48 @@
-////Audiiostreamcapture.h
-//#include <Windows.h>
-//#include <mmdeviceapi.h>
-//#include <wrl/client.h>
-//#include <Audioclient.h>
-//#include <string>
-//
-//using Microsoft::WRL::ComPtr;
-//class AudioStreamCapture {
-//private:
-//	ComPtr<IMMDeviceEnumerator> m_deviceEnumerator = nullptr;
-//	ComPtr<IMMDevice> m_device = nullptr;
-//	ComPtr<IAudioClient> m_audioClient = nullptr;
-//	ComPtr<IAudioCaptureClient> m_captureClient = nullptr;
-//	WAVEFORMATEX* m_waveFormat = nullptr;
-//public:
-//	bool Initialize();
-//	void Start(const char* outputFileName);
-//};
+#pragma once
+#include <Windows.h>
+#include <mmdeviceapi.h>
+#include <wrl/client.h>
+#include <Audioclient.h>
+#include <mutex>
+#include <atomic>
+#include <iostream>
+#include <memory>
+#include "AudioData.h"
+using Microsoft::WRL::ComPtr;
+
+class AudioStreamCapture {
+private:
+    static std::mutex mtx;
+    static AudioStreamCapture* instance;
+    bool Initialize();
+    AudioStreamCapture() {}
+
+protected:
+    ComPtr<IMMDeviceEnumerator> m_deviceEnumerator = nullptr;
+    ComPtr<IMMDevice> m_device = nullptr;
+    ComPtr<IAudioClient> m_audioClient = nullptr;
+    ComPtr<IAudioCaptureClient> m_captureClient = nullptr;
+    WAVEFORMATEX* m_waveFormat = nullptr;
+
+public:
+    ~AudioStreamCapture();
+    AudioStreamCapture(const AudioStreamCapture&) = delete;
+    AudioStreamCapture& operator=(const AudioStreamCapture&) = delete;
+
+    static AudioStreamCapture* GetInstance() {
+        if (!instance) {
+            std::lock_guard<std::mutex> lock(mtx);
+            if (!instance) {
+                instance = new AudioStreamCapture();
+                if (!instance->Initialize()) {
+                    std::cerr << "Failed to initialize audio stream capture" << std::endl;
+                    return nullptr;
+                }
+            }
+        }
+        return instance;
+    }
+    void StopStream();
+    void StartStream();
+    std::unique_ptr<AudioData> CaptureAudio();
+};
